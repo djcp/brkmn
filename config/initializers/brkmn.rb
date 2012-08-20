93353f91a6fa2029164d430f85a5365e7d08e3ab
Dan Collis-Puro <djcp@thoughtbot.com>
2012-08-20 10:08:37 -0400

    Update config.

--- config/initializers/brkmn.rb
+++ config/initializers/brkmn.rb
@@ -1,3 +1,3 @@
 PROTECTED_URL_REGEX = /^(url|user|metric|redirector|preview)/i
-PROTECTED_REDIRECT_REGEX = /^https?:\/\/(localhost|brk\.mn)/i
-REDIRECT_DOMAIN = 'brk.mn'
+PROTECTED_REDIRECT_REGEX = /^https?:\/\/(localhost|brkmn\.herokuapp\.com)/i
+REDIRECT_DOMAIN = 'brkmn.herokuapp.com'
